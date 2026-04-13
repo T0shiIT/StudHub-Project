5009fc55a751007c2018d80b5d2cadcd4c46bf48
Hxxb <kenumi.re@gmail.com>
2026-04-13 03:05:52 +0300

    Можно добавлять пользователей в базу данных

---
+++ database/handlers/new_user.h
@@ -0,0 +1,9 @@
+#pragma once
+#include "crow.h"
+
+namespace handlers {
+    void register_new_user_handler(crow::SimpleApp& app);
+}
+
+
+
--- database/main.cpp
+++ database/main.cpp
@@ -3,18 +3,24 @@
 #include <pqxx/pqxx>
 #include <crow.h>
 #include <string>
+#include "handlers/new_user.h"
 
-extern const std::string DB_CONN = "host=postgres-db dbname=TestAppLogic user=postgres password=postgres";
-
+std::string DB_CONN = "host=db dbname=studhub user=user password=pass";
 
 int main() {
-    crow::SimpleApp app;    
-
-    app.port(8081).multithreaded().run();
+    crow::SimpleApp app; 
     /*
     Вся реализация
     */
+    handlers::register_new_user_handler(app);
+
+
+    app.port(8081).multithreaded().run();
     return 0;
-
-
 }
+
+//проверка контакта что код может парсить java
+/*
+docker exec -it studhub-project-cpp-1 bash
+curl -X POST http://localhost:8081/api/cpp/sync-user -H "Content-Type: application/json" -d '{"id": "yandex_777", "default_email": "boss@yandex.ru"}'
+*/
