fa52a28842e37555fde3808e158b9b846408a318
T0shiIT <toshikek@vk.com>
2026-05-18 01:10:50 +0300

    fixe schedule.tsx

--- frontend/src/pages/schedule.tsx
+++ frontend/src/pages/schedule.tsx
@@ -132,7 +132,7 @@ export default function Schedule() {
                     {cell !== null ? cell : ''}
                   </td>
                 ))}
-              </table>
+              </tr>
             ))}
           </tbody>
         </table>
