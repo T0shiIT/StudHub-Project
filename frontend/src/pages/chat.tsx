c1e368af78737806d943eca4d492cfd82f561774
T0shiKek <toshikek@vk.com>
2026-06-11 17:37:10 +0300

    Update
    Убран общий чат

--- frontend/src/pages/chat.tsx
+++ frontend/src/pages/chat.tsx
@@ -8,7 +8,6 @@ const ChatPage: React.FC = () => {
 
   return (
     <div style={{ padding: '24px' }}>
-      <Chat roomId="general" title="Общий чат" />
       {user?.group && (
         <Chat roomId={`group-${user.group}`} title={`Чат группы ${user.group}`} />
       )}
